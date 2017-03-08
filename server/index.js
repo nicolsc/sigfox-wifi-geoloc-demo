1529d46f62ccd4d441818065f5964098e0831b5d
nicolsc <nicolas.lesconnec@gmail.com>
2017-03-08 14:48:23 +0100

    Init spotit route

--- server/index.js
+++ server/index.js
@@ -34,6 +34,19 @@ server.route({
     });
   }
 });
+server.route({
+  method:'POST',
+  path:'/locations/spotit',
+  handler:function(request, reply){
+    console.log('[%s] %s', request.method, request.path, request.payload);
+    if (request.payload.lat!==undefined|| request.payload.lng!==undefined || request.payload.radius!==undefined){
+      return reply("❌").code(400);
+    }
+    //Sigfox callback : reply 200 as soon as possible
+    reply("☺");
+
+  }
+});
 // Start the server
 server.start((err) => {
 
